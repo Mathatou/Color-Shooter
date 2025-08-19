3ec3e855425155d132c0cf8fd66fa3d01f7b9395
Mathatou <mathias.aubry@edu.esiee.fr>
2025-08-19 23:20:12 +0200

    Spawn les objet dans la scène en l'air

--- Color Shooter/Assets/Scripts/TargetSpawner.cs
+++ Color Shooter/Assets/Scripts/TargetSpawner.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 public class TargetSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] prefabTargets;
-    [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform[] spawnPosition;
+    private int[] randomSpawnIndex;
+    private int numberToSpawn = 10;
 
     public void SpawnTarget()
     {
-        int randomIndex = Random.Range(0, prefabTargets.Length);
-        Instantiate(prefabTargets[randomIndex],playerPosition);
-        
+        /// Cette loop permet de récup les différentes positions de manière aléatoire sans qu'elle ne se chevauchent
+        randomSpawnIndex = new int[numberToSpawn];
+        for (int i = 0; i < numberToSpawn; i++) 
+        {
+            randomSpawnIndex[i] = Random.Range(0, spawnPosition.Length);
+            for (int j = 0; j < i; j++) 
+            {
+                if (randomSpawnIndex[i] == randomSpawnIndex[j])
+                {
+                    randomSpawnIndex[i] = Random.Range(0, spawnPosition.Length);
+                    j = -1; // Restart la loop
+                }
+            }
+            int randomTargetIndex = Random.Range(0, prefabTargets.Length);
+            Instantiate(prefabTargets[randomTargetIndex], spawnPosition[randomSpawnIndex[i]].transform);
+        }
+
     }
+
     public void sendbeug()
     {
         Debug.Log("desactivtté");
