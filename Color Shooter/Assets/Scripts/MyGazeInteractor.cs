6db328935db6d2aba34f1c570cb12349447be156
Mathatou <mathias.aubry@edu.esiee.fr>
2025-10-02 00:11:19 +0200

    Add cycling tutorial text and tweak gaze dwell time
    Introduces a cycling tutorial text feature using TextMeshProUGUI in MyGazeInteractor, allowing the player to advance instructions by gazing at the Canvas. Also updates the GameScene to support the new text objects, corrects some text, and reduces the gaze dwell time from 10 to 3 seconds for improved responsiveness.

--- Color Shooter/Assets/Scripts/MyGazeInteractor.cs
+++ Color Shooter/Assets/Scripts/MyGazeInteractor.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class MyGazeInteractor : MonoBehaviour
@@ -6,10 +7,19 @@ public class MyGazeInteractor : MonoBehaviour
     [SerializeField] private float maxDistance = 100f;
     [SerializeField] private float dwellTime = 10f;
     [SerializeField] private LayerMask TargetLayer;
-    
+
+    private string[] textsToCycle = { 
+    "Bienvenue dans Color Shooter !", 
+    "Tirez sur les cibles de la couleur adéquate !", 
+    "Derrière vous se trouve deux pistolets de couleur.",
+    "Tirez le levier à côté pour lancer le jeu !"};
+
+
     private Camera mainCam;
     private float gazeTimer;
     private Transform currentTarget;
+    private int currentTextIndex = 0;
+    private TextMeshProUGUI textZone;
 
     private void Awake()
     {
@@ -22,14 +32,21 @@ public class MyGazeInteractor : MonoBehaviour
         if (Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit rHit, maxDistance,TargetLayer))
         {
             var targetTransform = rHit.transform;
-            Debug.Log($"targetTransform : {targetTransform} currentTarget : {currentTarget}");
             if (targetTransform == currentTarget)
             {
                 gazeTimer += Time.deltaTime;
                 if(gazeTimer >= dwellTime)
                 {
-                    Debug.Log("Gaze selected : " + targetTransform.name);
-                    rHit.collider.gameObject.SetActive(false);
+                    if (targetTransform.name.Equals("Canvas"))
+                    {
+                        if (textZone == null)
+                        {
+                            textZone = targetTransform.GetComponentInChildren<TextMeshProUGUI>();
+                        }
+                        currentTextIndex = (currentTextIndex + 1) % textsToCycle.Length;
+                        textZone.text = textsToCycle[currentTextIndex];
+                    }
+                    //rHit.collider.gameObject.SetActive(false);
                     gazeTimer = 0f;
                 }
             }
